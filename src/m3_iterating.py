72b8d1d90a786a291ae14811feabd153b6baf0a0
huh2 <huh2@rose-hulman.edu>
2017-12-15 12:56:23 -0500

    DONE

--- src/m3_iterating.py
+++ src/m3_iterating.py
@@ -214,6 +214,12 @@ def count_short_ones(seq_of_lists):
     # TODO: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
+    count = 0
+    for k in range(len(seq_of_lists)):
+        if len(seq_of_lists[k]) < 3:
+            count +=1
+
+    return count
 
 
 def run_test_draw_circles():
